92c7f45b0a108ff90c8b71dce7f1d467834a6945
Ali Haider <88906668+AliHaider083@users.noreply.github.com>
2025-03-25 20:32:52 +0500

    update cors
    update cors

--- superMoverBackend/settings.py
+++ superMoverBackend/settings.py
@@ -86,6 +86,9 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Allow Vite frontend
+     "https://proud-pond-0e49cde1e.6.azurestaticapps.net",
+    "http://127.0.0.1:8080",
+    "https://supermover-backend.azurewebsites.net"
 ]
 
 CORS_ALLOW_HEADERS = [
